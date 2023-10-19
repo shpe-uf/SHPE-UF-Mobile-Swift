6c5351f6b3dfa2ce2ddae61a1dd7ba97244e61a9
jesuslopez <jlopezmarti20@gmail.com>
2023-10-19 11:24:25 -0400

    Initial commit

---
+++ SHPE-UF-Mobile-Swift/SHPE-UF-Mobile-Swift/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SHPE-UF-Mobile-Swift
+//
+//  Created by Jesus Lopez on 10/19/23.
+//
+
+import SwiftUI
+// This is a test
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SHPE-UF-Mobile-Swift/SHPE-UF-Mobile-Swift/SHPE_UF_Mobile_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SHPE_UF_Mobile_SwiftApp.swift
+//  SHPE-UF-Mobile-Swift
+//
+//  Created by Jesus Lopez on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SHPE_UF_Mobile_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
