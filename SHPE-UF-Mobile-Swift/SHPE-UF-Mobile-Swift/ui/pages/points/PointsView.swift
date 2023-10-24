ce339c7ef9addc4d01d2e9f15916896ed305fc60
jesuslopez <jlopezmarti20@gmail.com>
2023-10-23 21:02:05 -0400

    Updated files

---
+++ SHPE-UF-Mobile-Swift/SHPE-UF-Mobile-Swift/ui/pages/points/PointsView.swift
@@ -0,0 +1,18 @@
+//
+//  PointsView.swift
+//  SHPE-UF-Mobile-Swift
+//
+//  Created by Jesus Lopez on 10/23/23.
+//
+
+import SwiftUI
+
+struct PointsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PointsView()
+}
