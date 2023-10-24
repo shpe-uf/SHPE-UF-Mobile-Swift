ae4c14dcd9e8b4ba8891f70e4c7932794aac176b
jesuslopez <jlopezmarti20@gmail.com>
2023-10-23 21:03:20 -0400

    updated files

--- SHPE-UF-Mobile-Swift/SHPE-UF-Mobile-Swift/SHPE_UF_Mobile_SwiftApp.swift
+++ SHPE-UF-Mobile-Swift/SHPE-UF-Mobile-Swift/SHPE_UF_Mobile_SwiftApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SHPE_UF_Mobile_SwiftApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SHPEUFAppView()
         }
     }
 }
