edfc2a612fa8742aa936e701b1e4261ddbcbd9a9
DaniParr <daniel27parra@gmail.com>
2023-10-26 18:11:23 -0400

    view model and model instantiation

--- SHPE-UF-Mobile-Swift/SHPE-UF-Mobile-Swift/data/Models/SHPEito.swift
+++ SHPE-UF-Mobile-Swift/SHPE-UF-Mobile-Swift/data/Models/SHPEito.swift
@@ -2,6 +2,38 @@
 import Foundation
 
 
-class SHPEito {
-    
+class SHPEito: Identifiable {
+    var id:UUID = UUID()
+    var firstName: String = ""
+    var lastName: String = ""
+    var photo: String = ""
+    var major: String = ""
+    var year: String = ""
+    var graduating: String = ""
+    var country: String = ""
+    var ethnicity: String = ""
+    var sex: String = ""
+    var username: String = ""
+    var email: String = ""
+    private var password: String = ""
+    var createdAt: String = ""
+    var updatedAt: String = ""
+    var points: Int = 0
+    var fallPoints: Int = 0
+    var springPoints: Int = 0
+    var summerPoints: Int = 0
+    var fallPercentile: Int = 0
+    var springPercentile: Int = 0
+    var summerPercentile: Int = 0
+    var permission: String = ""
+    var listServ: Bool = false
+    var internships: [String] = []
+    var socialMedia: [String] = []
+//    var events: [Event]
+//    var tasks: [Task]
+    var bookmarkedTasks: [String] = []
+    var classes: [String] = []
+    var token: String = ""
+    var confirmed: Bool = false
+    var bookmarks: [String] = []
 }
