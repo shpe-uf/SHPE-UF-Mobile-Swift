093919efd9a24322fcda4de33b2e108942463381
DaniParr <daniel27parra@gmail.com>
2024-02-08 18:51:37 -0500

    Added fetchEvents function and fetch events sample button to test function. Also added Event Model

---
+++ SHPE-UF-Mobile-Swift/SHPE-UF-Mobile-Swift/data/Models/Event.swift
@@ -0,0 +1,44 @@
+//
+//  Event.swift
+//  SHPE-UF-Mobile-Swift
+//
+//  Created by Daniel Parra on 2/8/24.
+//
+
+import Foundation
+
+import Foundation
+
+struct Event {
+    let created: Date
+    let creator: Creator
+    let end: EventDateTime
+    let etag: String
+    let eventType: String
+    let htmlLink: String
+    let iCalUID: String
+    let id: String
+    let kind: String?
+    let organizer: Organizer
+    let sequence: Int
+    let start: EventDateTime
+    let status: String
+    let summary: String
+    let updated: Date
+    let location: String?
+}
+
+struct Creator {
+    let email: String
+    let selfValue: Int
+}
+
+struct EventDateTime {
+    let dateTime: Date
+    let timeZone: String
+}
+
+struct Organizer {
+    let email: String
+    let selfValue: Int
+}
