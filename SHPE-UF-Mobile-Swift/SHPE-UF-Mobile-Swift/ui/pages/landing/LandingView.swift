00fdde44f4045db402ab7caa0a49691a9bcc96b1
gabymilgram <gabriellamilgram@ufl.edu>
2024-02-29 17:25:27 -0500

    created LandingView file

---
+++ SHPE-UF-Mobile-Swift/SHPE-UF-Mobile-Swift/ui/pages/landing/LandingView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingView.swift
+//  SHPE-UF-Mobile-Swift
+//
+//  Created by Gaby Milgram on 2/29/24.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}
