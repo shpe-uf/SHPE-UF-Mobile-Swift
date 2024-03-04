3e60d6eed4ec271cf929b7b80ac1ea8016186dc2
davidDenis17 <davidalejandro1.dadg@gmail.com>
2024-03-04 18:30:21 -0500

    removed tracking from unnecessary files

--- SHPE-UF-Mobile-Swift/SHPE-UF-Mobile-Swift/ui/pages/points/PointsView.swift
+++ SHPE-UF-Mobile-Swift/SHPE-UF-Mobile-Swift/ui/pages/points/PointsView.swift
@@ -109,7 +109,12 @@ struct PointsView: View {
                 .padding(.vertical)
                 
                 
-                TableView()
+                VStack(spacing: 35) {
+                    TableView()
+                    TableView(title: "SOCIALS")
+                    TableView(title: "CABINET\nMEETING")
+                }
+                .padding()
                     
             }
         }
@@ -146,6 +151,29 @@ struct PointsView: View {
 }
 
 #Preview {
-    PointsView(vm: PointsViewModel(shpeito: SHPEito()))
+    PointsView(vm: PointsViewModel(shpeito:
+                                    SHPEito(
+                                        username: "dvera0322",
+                                            password: "",
+                                            remember: "true",
+                                            photo: "",
+                                            firstName: "David",
+                                            lastName: "Denis",
+                                            year: "2",
+                                            major: "Computer Science",
+                                            id: "642f7f80e8839f0014e8be9b",
+                                            token: "",
+                                            confirmed: true,
+                                            updatedAt: "",
+                                            createdAt: "",
+                                            email: "denisdavid@ufl.edu",
+                                            fallPoints: 20,
+                                            summerPoints: 17,
+                                            springPoints: 30,
+                                            points: 67,
+                                            fallPercentile: 93,
+                                            springPercentile: 98,
+                                            summerPercentile: 78)
+                                  ))
     .preferredColorScheme(.light)
 }
