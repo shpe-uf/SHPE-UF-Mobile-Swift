75e6e1572039b8d8eab2e2bf1fdb21f06dae61aa
Vivian Rincon <vivirincond@gmail.com>
2024-03-07 17:42:27 -0500

    first commit

---
+++ SHPE-UF-Mobile-Swift/SHPE-UF-Mobile-Swift/ui/pages/profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SHPE-UF-Mobile-Swift
+//
+//  Created by victoria dib on 3/7/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
