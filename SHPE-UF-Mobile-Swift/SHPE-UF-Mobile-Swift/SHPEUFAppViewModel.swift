2108ccc8b0b18f9993cbb358dd596ca0f5660b42
Daniel Parra <daniel27parra@gmail.com>
2024-03-27 17:45:46 -0400

    Finalized the edit user page

--- SHPE-UF-Mobile-Swift/SHPE-UF-Mobile-Swift/SHPEUFAppViewModel.swift
+++ SHPE-UF-Mobile-Swift/SHPE-UF-Mobile-Swift/SHPEUFAppViewModel.swift
@@ -6,20 +6,32 @@
 //
 
 import Foundation
+import SwiftUI
 
 class AppViewModel: ObservableObject
 {
     static let appVM = AppViewModel()
     @Published var pageIndex:Int
     @Published var shpeito:SHPEito
+    @Published var darkMode:Bool
     
     private init() {
         self.pageIndex = -1
         self.shpeito = SHPEito()
+        self.darkMode = false
     }
     
     public func setPageIndex(index:Int)
     {
         self.pageIndex = index
+    }
+    
+    public func setDarkMode(bool:Bool, user: FetchedResults<User>)
+    {
+        self.darkMode = bool
+        if !user.isEmpty
+        {
+            user[0].darkMode = bool
+        }
     }
 }
