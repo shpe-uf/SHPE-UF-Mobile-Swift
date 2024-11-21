21465c2a5c80337a508870faea307827075bc6bd
habibimazin <mazinsameersaleh@gmail.com>
2024-11-21 17:26:45 -0500

    Add QR code functionality in qrcodescannerview, added assets for qr code ui (back button and qr code bound square), and added camera button to redeem view

--- SHPE-UF-Mobile-Swift/GraphQL/RequestHandler.swift
+++ SHPE-UF-Mobile-Swift/GraphQL/RequestHandler.swift
@@ -11,8 +11,8 @@ import Apollo
 
 class RequestHandler
 {
-    let apolloClient = ApolloClient(url: URL(string: ProcessInfo.processInfo.environment["SERVER_LINK"]!)!) // MUST BE NGROK URL or http://127.0.0.1:5000/
-    
+    let apolloClient = ApolloClient(url: URL(string: "https://83a9-128-227-1-17.ngrok-free.app")!) // MUST BE NGROK URL or //http://127.0.0.1:5000/
+    //ProcessInfo.processInfo.environment["SERVER_LINK"]!
     // MARK: Example Query Function
     // This is how the functions I will make for you guys will look like
     func fetchUserPoints(userId:String, completion: @escaping ([String:Any])->Void)
