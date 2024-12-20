133aeb745785d8ce39e7236aa0fccb871be45130
Daniel Parra <daniel27parra@gmail.com>
2024-12-20 15:26:18 -0500

    Made it so the corners of the QR code detection scale to where the QR code is located in the camera. Added a delay of 1 second so users can appreciate QR code scanning and our ability to pinpoint where the QR code is!

--- SHPE-UF-Mobile-Swift/SHPE-UF-Mobile-Swift/ui/pages/points/QRCodeScannerView.swift
+++ SHPE-UF-Mobile-Swift/SHPE-UF-Mobile-Swift/ui/pages/points/QRCodeScannerView.swift
@@ -6,6 +6,8 @@ struct QRCodeScannerView: View {
     @Binding var scannedCode: String
     @State private var showAlert = false
     @State private var isScannerActive = true // Controls the visibility of the scanner
+    @State private var corners: [CGPoint] = []
+    @State private var image:UIImage? = nil
 
     var body: some View {
         ZStack {
@@ -16,14 +18,23 @@ struct QRCodeScannerView: View {
                     completion: { result in
                         switch result {
                         case .success(let code):
-                            if code.string.starts(with: "[shpeuf]:") {
-                                // Extract the portion after the prefix
-                                let validCode = code.string.replacingOccurrences(of: "[shpeuf]:", with: "")
-                                scannedCode = validCode
-                                dismiss()
-                            } else {
-                                showAlert = true
-                                isScannerActive = false // Deactivate scanner
+                            /// Take the corners from the code message
+                            corners = code.corners
+                            image = code.image
+                            /// Place the corners over those points in the image and wait 2 seconds to check if the QR code is valid
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1)
+                            {
+                                if code.string.starts(with: "[SHPEUF]:") {
+                                    // Extract the portion after the prefix
+                                    let validCode = code.string.replacingOccurrences(of: "[SHPEUF]:", with: "")
+                                    scannedCode = validCode
+                                    image = nil
+                                    dismiss()
+                                } else {
+                                    showAlert = true
+                                    isScannerActive = false // Deactivate scanner
+                                    image = nil
+                                }
                             }
                         case .failure:
                             isScannerActive = false // Reset scanner on failure
@@ -55,42 +66,83 @@ struct QRCodeScannerView: View {
                     .padding(.top, 40)
 
                     Spacer()
+                }
+                .zIndex(999)
+                
+                let width = geometry.size.width * 0.6
+                let height = width
+                let xOffset = (geometry.size.width - width) / 2
+                let yOffset = (geometry.size.height - height) / 2
 
-                    // Corner Images Overlay
+                // Top-Left Corner
+                Image("qr_code_top_left_corner")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .position(x: xOffset, y: yOffset)
+
+                // Top-Right Corner
+                Image("qr_code_top_right_corner")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .position(x: xOffset + width, y: yOffset)
+
+                // Bottom-Left Corner
+                Image("qr_code_bottom_left_corner")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .position(x: xOffset, y: yOffset + height)
+
+                // Bottom-Right Corner
+                Image("qr_code_bottom_right_corner")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .position(x: xOffset + width, y: yOffset + height)
+                
+                // Corner Images Overlay
+                if image != nil
+                {
+                    // Calculate scaling factors to fit the image within the screen
+                    let imageAspectRatio = image!.size.width / image!.size.height
+                    let screenAspectRatio = geometry.size.width / geometry.size.height
+                    
+                    let scale: CGFloat = imageAspectRatio > screenAspectRatio ? geometry.size.height / image!.size.height : geometry.size.width / image!.size.width
+                    let offsetX: CGFloat = imageAspectRatio > screenAspectRatio ? (geometry.size.width - (image!.size.width * scale)) / 2 :  0
+                    let offsetY: CGFloat = imageAspectRatio > screenAspectRatio ? 0 : (geometry.size.height - (image!.size.height * scale)) / 2
+                    
+                    
                     ZStack {
                         // Scanning Area Dimensions
-                        let width = geometry.size.width * 0.6
-                        let height = width
-                        let xOffset = (geometry.size.width - width) / 2
-                        let yOffset = (geometry.size.height - height) / 2 - 90
-
-                        // Top-Left Corner
-                        Image("qr_code_top_left_corner")
+                        // Display the image
+                        Image(uiImage: image!)
                             .resizable()
-                            .frame(width: 40, height: 40)
-                            .position(x: xOffset, y: yOffset)
-
-                        // Top-Right Corner
-                        Image("qr_code_top_right_corner")
-                            .resizable()
-                            .frame(width: 40, height: 40)
-                            .position(x: xOffset + width, y: yOffset)
-
-                        // Bottom-Left Corner
-                        Image("qr_code_bottom_left_corner")
-                            .resizable()
-                            .frame(width: 40, height: 40)
-                            .position(x: xOffset, y: yOffset + height)
-
-                        // Bottom-Right Corner
-                        Image("qr_code_bottom_right_corner")
-                            .resizable()
-                            .frame(width: 40, height: 40)
-                            .position(x: xOffset + width, y: yOffset + height)
+                            .scaledToFill()
+                            .ignoresSafeArea()
+                        
+                        // Place corners on image
+                        let width:CGFloat = geometry.size.width
+                        let height:CGFloat = geometry.size.height
+                        
+                        let cornerAttributes: [String] = ["qr_code_top_left_corner", "qr_code_bottom_left_corner", "qr_code_bottom_right_corner", "qr_code_top_right_corner"]
+                        
+                        ForEach(corners.indices, id:\.self)
+                        {
+                            corner_i in
+                            
+                            let rescale_factor = 1.04
+                            let x = (1-corners[corner_i].y) * (image!.size.width * scale) * rescale_factor
+                            let y = (corners[corner_i].x) * (image!.size.height * scale) * rescale_factor
+                            
+                            Image(cornerAttributes[corner_i])
+                                .resizable()
+                                .frame(width: 10, height: 10)
+                                .position(x: x, y: y)
+                        }
                     }
+                    .zIndex(100)
                 }
             }
         }
+        .background(Color.black)
         .alert(isPresented: $showAlert) {
             Alert(
                 title: Text("Invalid Code"),
