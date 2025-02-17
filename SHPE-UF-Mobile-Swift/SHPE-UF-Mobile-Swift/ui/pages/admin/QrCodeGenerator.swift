a7c6882408a329b5d8f8a313a57e4fba1f116263
Evanlsmillan <evanschrdr@gmail.com>
2025-02-17 14:48:00 -0500

    QRCode generator

---
+++ SHPE-UF-Mobile-Swift/SHPE-UF-Mobile-Swift/ui/pages/admin/QrCodeGenerator.swift
@@ -0,0 +1,45 @@
+//
+//  Untitled.swift
+//  SHPEsettingthingsup
+//
+//  Created by Evan Schroeder on 2/9/25.
+//
+import Foundation
+import SwiftUI
+import CoreImage.CIFilterBuiltins
+
+struct QRTestView: View {
+    var code: String
+    
+    var body: some View {
+        Image(uiImage: QrCodeGenerator(eventCode: code))
+            .interpolation(.none)
+            .resizable()
+            .frame(width: 150, height: 150, alignment: .center)            
+            
+    }
+}
+
+func QrCodeGenerator(eventCode: String) -> UIImage {
+    // Contatenate string
+    let qrString = "[SHPEUF]:" + eventCode
+    
+    // This actually sets up teh qr code stuff
+    let qrCodeGenerator = CIFilter.qrCodeGenerator()
+    let data = qrString.data(using:.ascii)!
+    let context = CIContext()
+    qrCodeGenerator.setValue(data, forKey: "inputMessage")
+    
+    //Generates the image
+    if let qrCodeImage = qrCodeGenerator.outputImage{
+        if let qrCodeCGImage = context.createCGImage(qrCodeImage, from: qrCodeImage.extent){
+            return UIImage(cgImage: qrCodeCGImage)
+        }
+    }
+    return UIImage(systemName: "xmark") ?? UIImage()
+    
+    
+}
+
+
+
