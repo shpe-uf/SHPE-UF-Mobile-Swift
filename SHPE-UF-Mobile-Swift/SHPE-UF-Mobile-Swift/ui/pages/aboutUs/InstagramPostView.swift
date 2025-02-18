93b8de3a2176b3aa76b0b8485c8241aced0f0dc1
davidDenis17 <davidalejandro1.dadg@gmail.com>
2025-02-18 17:32:02 -0500

    Insta Model & Post Carousel
    Implemented the Instagram Post Model and Created Post Carousel

---
+++ SHPE-UF-Mobile-Swift/SHPE-UF-Mobile-Swift/ui/pages/aboutUs/InstagramPostView.swift
@@ -0,0 +1,54 @@
+//
+//  InstagramPostView.swift
+//  SHPE-UF-Mobile-Swift
+//
+//  Created by David Denis on 2/18/25.
+//
+
+import SwiftUI
+
+struct InstagramPostView: View {
+    var body: some View {
+        
+        ZStack {
+            
+            
+            VStack(alignment: .leading) {
+                HStack(spacing: 5) {
+                    Image("swift.shpelogo")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .clipShape(Circle())
+                    
+                    Text("@shpeuf")
+                        .font(.subheadline)
+                        .foregroundColor(.primary)
+                }
+                
+             
+                
+                Rectangle()
+                    .opacity(0.2)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                
+                HStack {
+                    Text("shpeuf")
+                        .font(.headline)
+                        .bold()
+                    
+                    Text("...")
+                }
+                .padding(.leading, 3)
+                
+            }
+            .padding()
+            
+            
+            
+        }
+    }
+}
+
+#Preview {
+    InstagramPostView()
+}
